58601453598b8ad67c047aa672cf0d79dee1e269
haha <haha@kgc.cn>
2019-11-26 15:22:03 +0800

    添加到本地仓库

--- ideas-git-test/src/com/kgc/Hello.java
+++ ideas-git-test/src/com/kgc/Hello.java
@@ -5,5 +5,7 @@ public class Hello {
         System.out.println("Commit");
         System.out.println("Commit");
         System.out.println("Commit");
+        System.out.println("Commit");
+        System.out.println("Commit");
     }
 }
